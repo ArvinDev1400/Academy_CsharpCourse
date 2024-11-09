c96f0427eb4629fc93323ed3eafee1796f537bde
ArvinDev1400 <arvindev1400@gmail.com>
2024-11-09 20:25:02 +0330

    Completed if else section

--- 3_If_Else/Program.cs
+++ 3_If_Else/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = 10;
+//number++;
+//number--;
+
+number %= 4;
+
+Console.WriteLine(number);
+Console.ReadKey();
+
+
+Console.WriteLine("please enter number of week:");
+int numberOfWeek = int.Parse(Console.ReadLine());
+
+if (numberOfWeek == 1)
+    Console.WriteLine("Saturday");
+
+else if (numberOfWeek == 2)
+    Console.WriteLine("Sunday");
+
+else if (numberOfWeek == 3)
+    Console.WriteLine("Monday");
+
+else if (numberOfWeek == 4)
+    Console.WriteLine("Tuesday");
+
+else if (numberOfWeek == 5)
+    Console.WriteLine("Wednesday");
+
+else if (numberOfWeek == 6)
+    Console.WriteLine("Thursday");
+
+else if (numberOfWeek == 7)
+    Console.WriteLine("Friday");
+
+else if (numberOfWeek < 1 && numberOfWeek > 7)
+    Console.WriteLine();
+
+Console.ReadKey();
