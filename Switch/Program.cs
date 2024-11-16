83157783394679dd6248d5f820690af3a4728e1e
ali-naderifar <alinaderifar213@gmail.com>
2024-11-16 20:09:36 +0330

    complete example one for switch case

--- Switch/Program.cs
+++ Switch/Program.cs
@@ -1,39 +1,86 @@
-﻿Console.WriteLine("please enter number of week:");
-int numberOfWeek = int.Parse(Console.ReadLine());
+﻿
+#region Week
 
-switch (numberOfWeek)
+//Console.WriteLine("please enter number of week:");
+//int numberOfWeek = int.Parse(Console.ReadLine());
+//switch (numberOfWeek)
+//{
+//    case 1:
+//        Console.WriteLine("Saturday");
+//        break;
+
+//    case 2:
+//        Console.WriteLine("Sunday");
+//        break;
+
+//    case 3:
+//        Console.WriteLine("Monday");
+//        break;
+
+//    case 4:
+//        Console.WriteLine("Tuesday");
+//        break;
+
+//    case 5:
+//        Console.WriteLine("Wednesday");
+//        break;
+
+//    case 6:
+//        Console.WriteLine("Thursday");
+//        break;
+
+//    case 7:
+//        Console.WriteLine("Friday");
+//        break;
+
+//    default:
+//        Console.WriteLine("number incorrect");
+//        break;
+//}
+
+#endregion
+
+try
 {
-    case 1:
-        Console.WriteLine("Saturday");
-        break;
 
-    case 2:
-        Console.WriteLine("Sunday");
-        break;
+    Console.WriteLine("please enter first number:");
+    int number1 = int.Parse(Console.ReadLine());
 
-    case 3:
-        Console.WriteLine("Monday");
-        break;
 
-    case 4:
-        Console.WriteLine("Tuesday");
-        break;
+    Console.WriteLine("please enter second number:");
+    int number2 = int.Parse(Console.ReadLine());
 
-    case 5:
-        Console.WriteLine("Wednesday");
-        break;
+    Console.WriteLine("please enter one operator. Example: '*' '/' '+' '-'");
+    string operation = Console.ReadLine();
 
-    case 6:
-        Console.WriteLine("Thursday");
-        break;
+    switch (operation)
+    {
+        case "/":
+            Console.WriteLine(number1 / number2);
+            break;
 
-    case 7:
-        Console.WriteLine("Friday");
-        break;
+        case "*":
+            Console.WriteLine(number1 * number2);
+            break;
 
-    default:
-        Console.WriteLine("number incorrect");
-        break;
+        case "-":
+            Console.WriteLine(number1 - number2);
+            break;
+
+        case "+":
+            Console.WriteLine(number1 + number2);
+            break;
+
+        default:
+            Console.WriteLine("my application not support this operation");
+            break;
+    }
+
+
+    Console.ReadKey();
 }
+catch (Exception)
+{
 
-Console.ReadKey();
+    Console.WriteLine("error: please recheck in input valus");
+}
