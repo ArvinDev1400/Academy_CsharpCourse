dd522b30aadf8eacbf621dae60b2588660e850a0
sedighe <sedighe.shekoohi@gmail.com>
2024-11-23 21:30:38 +0330

    Added: do_while project

--- 6_While/Program.cs
+++ 6_While/Program.cs
@@ -1,7 +1,35 @@
-﻿int count = 1;
+﻿//int count = 1;
 
-while (count < 10)
+//while (count < 10)
+//{
+//    Console.WriteLine(count);
+//    count++;
+//}
+
+Console.WriteLine("please enter the number");
+long number = long.Parse(Console.ReadLine());
+long i = number;
+long count = 0;
+
+while (i > 0)
 {
-    Console.WriteLine(count);
-    count++;
+    if (number % i == 0)
+    {
+
+        count++;
+    }
+
+    Console.WriteLine(i);
+    i--;
 }
+
+if (count == 2)
+{
+    Console.WriteLine("it's prime");
+}
+else
+{
+    Console.WriteLine("not prime");
+}
+
+Console.ReadKey();
---
+++ 7_do_while/Program.cs
@@ -0,0 +1,34 @@
+﻿//int x = 10;
+//do
+//{
+//    Console.WriteLine(x);
+//    x++;
+//} while (x < 5);
+
+int sum = 10;
+sum += 100;
+Console.WriteLine(sum);
+
+
+int x = 6;
+while (x < 10)
+{
+    Console.WriteLine(x);
+
+    if (x == 5)
+        break;
+
+    x++;
+}
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
