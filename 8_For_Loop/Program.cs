58989fa56c1924f513288719f694486e7b06a35d
ali-naderifar <alinaderifar213@gmail.com>
2024-11-30 21:01:06 +0330

    Added For_Loop Project

---
+++ 8_For_Loop/Program.cs
@@ -0,0 +1,16 @@
+﻿#region Repeate with while
+//int counter = 0;
+//while (counter < 10)
+//{
+//    Console.WriteLine("Mj.Arvin");
+//    counter++;
+//}
+#endregion
+
+for (int i = 0; i < 10; i++)
+{
+    if (i == 3)
+        continue;
+
+    Console.WriteLine(i);
+}
