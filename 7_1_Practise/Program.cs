d840feaa58f7480c74b2c69c408b720bd766ef31
ali-naderifar <alinaderifar213@gmail.com>
2024-11-30 20:31:59 +0330

    Add practise for While Loop

---
+++ 7_1_Practise/Program.cs
@@ -0,0 +1,22 @@
+﻿int inputCounter = 0;
+int number;
+int maxNumber;
+maxNumber = int.Parse(Console.ReadLine());
+while (inputCounter < 4)
+{
+    number = int.Parse(Console.ReadLine());
+    if (number > maxNumber)
+    {
+        maxNumber = number;
+    }
+    inputCounter++;
+}
+
+Console.WriteLine(maxNumber);
+
+
+
+
+
+
+Console.ReadKey();
