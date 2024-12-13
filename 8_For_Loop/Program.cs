721346e905025f32f8ec86d8cd8b677c7af37ca1
ali-naderifar <alinaderifar213@gmail.com>
2024-12-13 11:07:55 +0330

    Star Pattern

--- 8_For_Loop/Program.cs
+++ 8_For_Loop/Program.cs
@@ -1,4 +1,5 @@
-﻿#region Repeate with while
+﻿
+#region Repeate with while
 //int counter = 0;
 //while (counter < 10)
 //{
@@ -7,10 +8,53 @@
 //}
 #endregion
 
-for (int i = 0; i < 10; i++)
-{
-    if (i == 3)
-        continue;
+#region Continure
+//for (int i = 0; i < 10; i++)
+//{
+//    if (i == 3)
+//        continue;
 
-    Console.WriteLine(i);
+//    Console.WriteLine(i);
+//}
+#endregion
+
+#region Multiplication table
+//for (int i = 1; i <= 10; i++)
+//{
+//    for (int j = 0; j <= 10; j++)
+//    {
+//        Console.Write(i + " * " + j + " = " + i * j + " | ");
+//    }
+//    Console.WriteLine();
+//    Console.WriteLine();
+//}
+
+#endregion
+
+
+#region StarPattern
+
+Console.WriteLine();
+for (int i = 1; i <= 5; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
 }
+
+Console.WriteLine();
+for (int i = 1; i <= 5; i++)
+{
+    for (int j = 1; j <= 5; j++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}
+#endregion
+
+#region Fibonacci
+
+#endregion
